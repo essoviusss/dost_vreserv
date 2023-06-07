f7be1b74beae36a85f73f4f8675f1393a28ac64b
Abdullah Abaga <112831592+essoviusss@users.noreply.github.com>
2023-06-07 14:51:34 +0800

    Ab

--- src/pages/Driver/DrvRequestLogs.js
+++ src/pages/Driver/DrvRequestLogs.js
@@ -2,22 +2,17 @@ import '../Driver/components/DrvRequestLogs.css';
 import Header from "../../header/header";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-//material ui
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import { Table, TableBody, TableCell, TableContainer, TablePagination, TableRow } from '@mui/material';
 import Paper from '@mui/material/Paper';
-import '../Employee/components/EmpRequestLogs.css';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import { generateTripTicket } from '../../utils/pdfUtils';
 
-export default function DrvRequestLogs(){
+export default function DrvRequestLogs() {
   //font
   const theme = createTheme({
     typography: {
@@ -69,211 +64,215 @@ export default function DrvRequestLogs(){
 
       try {
         const response = await axios.post(url, fData);
-        console.log(userId);
-        if (Array.isArray(response.data.data) && response.data.message === "Success") {
+        if (Array.isArray(response.data.data)) {
           setRequest(response.data.data);
-        } else {
-          alert("Error");
         }
-      } catch (error) {
-        console.error('Error:', error);
-        alert("Error");
+      } catch (e) {
+        alert(e);
       }
-    }
+    };
 
     fetchData();
   }, [request]);
 
-    return(
-      <ThemeProvider theme={theme}>
-        <div className="page-container">
-          <Header/>
-          <div className="rlogs-text">Requests</div>
-          <Paper sx={{ borderRadius: '10px' }}>
-            <TableContainer>
-              <Table>
-                <thead>
-                  <tr>
-                    <th className='requestlog-th' style={{ textAlign: 'center' }}>Vehicle Number</th>
-                    <th className='requestlog-th' style={{ textAlign: 'center' }}>Vehicle Name</th>
-                    <th className='requestlog-th' style={{ textAlign: 'center' }}>Requested by</th>
-                    <th className='requestlog-th' style={{ textAlign: 'center' }}>Destination</th>
-                    <th className='requestlog-th' style={{ textAlign: 'center' }}>Status</th>
-                    <th className='requestlog-th' style={{ textAlign: 'center' }}>Action</th>
-                  </tr>
-                </thead>
-                <TableBody>
+  const handleGenerateTripTicket = async () => {
+    try {
+      await generateTripTicket(selectedRequest);
+    } catch (error) {
+      console.log('Error generating PDF:', error);
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="page-container">
+        <Header />
+        <div className="rlogs-text">Requests</div>
+        <Paper sx={{ borderRadius: '10px' }}>
+          <TableContainer>
+            <Table>
+              <thead>
+                <tr>
+                  <th className='requestlog-th' style={{ textAlign: 'center' }}>Vehicle Number</th>
+                  <th className='requestlog-th' style={{ textAlign: 'center' }}>Vehicle Name</th>
+                  <th className='requestlog-th' style={{ textAlign: 'center' }}>Requested by</th>
+                  <th className='requestlog-th' style={{ textAlign: 'center' }}>Destination</th>
+                  <th className='requestlog-th' style={{ textAlign: 'center' }}>Status</th>
+                  <th className='requestlog-th' style={{ textAlign: 'center' }}>Action</th>
+                </tr>
+              </thead>
+              <TableBody>
                 {request.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((request) => (
-                    <TableRow key={request.request_id}>
-                      <TableCell style={{ textAlign: 'center' }}>{request.vehicle_number}</TableCell>
-                      <TableCell style={{ textAlign: 'center' }}>{request.vehicle_name}</TableCell>
-                      <TableCell style={{ textAlign: 'center' }}>{request.requested_by}</TableCell>
-                      <TableCell style={{ textAlign: 'center' }}>{request.destination}</TableCell>
-                      <TableCell style={{ textAlign: 'center', padding: 0 }}>
-                        <div style={{ 
-                          backgroundColor: 
-                            request.request_status === "Pending" ? '#FDC858' :
+                  <TableRow key={request.request_id}>
+                    <TableCell style={{ textAlign: 'center' }}>{request.vehicle_number}</TableCell>
+                    <TableCell style={{ textAlign: 'center' }}>{request.vehicle_name}</TableCell>
+                    <TableCell style={{ textAlign: 'center' }}>{request.requested_by}</TableCell>
+                    <TableCell style={{ textAlign: 'center' }}>{request.destination}</TableCell>
+                    <TableCell style={{ textAlign: 'center', padding: 0 }}>
+                      <div style={{
+                        backgroundColor:
+                          request.request_status === "Pending" ? '#FDC858' :
                             request.request_status === "Approved" ? 'green' :
-                            request.request_status === "Disapproved" ? '#b21127' :
-                            request.request_status === "For Approval" ? '#025BAD' : 'inherit', 
-                          color: 'white',
-                          padding: '5px 5px',
-                          borderRadius: '50px',
-                          width: '80%', // Adjust as needed
-                          margin: 'auto',
-                        }}>
-                          {request.request_status}
-                        </div>
-                      </TableCell>
-                      <TableCell style={{ textAlign: 'center' }}>
-                        <Button variant="contained" onClick={() => handleOpenView(request)}>
-                          View Details
-                        </Button>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-              <TablePagination
-                rowsPerPageOptions={[5, 10, 25]}
-                component="div"
-                count={filteredRequests.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-              />
-            </TableContainer>
-          </Paper>
-      {/* view modal*/}
-      <Dialog open={openView} onClose={CloseView} fullWidth maxWidth="md">
-      <Button onClick={CloseView} style={{ color: 'gray', position: 'absolute', top: 10, right: 0, paddingLeft: 0, paddingRight: 0 }}>
-      <CloseRoundedIcon />
-      </Button>
-        <DialogContent>
-          <DialogContentText>
-          </DialogContentText>
-          <div className="div-drvreq">
-              <div className="div1-drvreq">
+                              request.request_status === "Disapproved" ? '#b21127' :
+                                request.request_status === "For Approval" ? '#025BAD' : 'inherit',
+                        color: 'white',
+                        padding: '5px 5px',
+                        borderRadius: '50px',
+                        width: '80%', // Adjust as needed
+                        margin: 'auto',
+                      }}>
+                        {request.request_status}
+                      </div>
+                    </TableCell>
+                    <TableCell style={{ textAlign: 'center' }}>
+                      <Button variant="contained" onClick={() => handleOpenView(request)}>
+                        View Details
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component="div"
+              count={filteredRequests.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </TableContainer>
+        </Paper>
+        {/* view modal*/}
+        <Dialog open={openView} onClose={CloseView} fullWidth maxWidth="md">
+          <Button onClick={CloseView} style={{ color: 'gray', position: 'absolute', top: 10, right: 0, paddingLeft: 0, paddingRight: 0 }}>
+            <CloseRoundedIcon />
+          </Button>
+          <DialogContent>
+            <DialogContentText>
+            </DialogContentText>
+              <div className="div-drvreq">
                 <div className="div1-drvreq">
-                  <img className="summary-logo" src="/images/summary_logo.png"/>
+                  <div className="div1-drvreq">
+                    <img className="summary-logo" src="/images/summary_logo.png" alt=''/>
+                  </div>
                 </div>
-              </div>
-              <div className="div2-drvreq">
-                <p className="header-drvreq">{selectedRequest.vehicle_name}</p>
-                <p className="header-label-drvreq">Vehicle to be requested</p>
-              </div>
-              <div className="div3-drvreq">
-                <p className="header-drvreq">{selectedRequest.driver_name}</p>
-                <p className="header-label-drvreq">Name of the driver</p>
-              </div>
-              <div className="status-button-container">
-                {selectedRequest && selectedRequest.request_status && (
-                  <button className={`status-button ${selectedRequest.request_status.replace(/\s+/g, '')}`}>
-                    {selectedRequest.request_status}
-                  </button>
-                )}
-              </div>
-              <div className="div5-drvreq">
-                <hr class="drvreq-hr"/>
-              </div>
-              <div className="div6-drvreq">
-                <p className="schedlabel-drvreq">SCHEDULE OF TRAVEL</p>
-              </div>
-              <div className="div7-drvreq">
-                <div className="div7table-drvreq">
-                <table>
-                  <tbody>
-                    <tr>
-                      <td className="table-label-drvreq">
-                        <p className="header-label-drvreq">Departure Date and Time:</p>
-                      </td>
-                      <td>
+                <div className="div2-drvreq">
+                  <p className="header-drvreq">{selectedRequest.vehicle_name}</p>
+                  <p className="header-label-drvreq">Vehicle to be requested</p>
+                </div>
+                <div className="div3-drvreq">
+                  <p className="header-drvreq">{selectedRequest.driver_name}</p>
+                  <p className="header-label-drvreq">Name of the driver</p>
+                </div>
+                <div className="status-button-container">
+                  {selectedRequest && selectedRequest.request_status && (
+                    <button className={`status-button ${selectedRequest.request_status.replace(/\s+/g, '')}`}>
+                      {selectedRequest.request_status}
+                    </button>
+                  )}
+                </div>
+                <div className="div5-drvreq">
+                  <hr class="drvreq-hr"/>
+                </div>
+                <div className="div6-drvreq">
+                  <p className="schedlabel-drvreq">SCHEDULE OF TRAVEL</p>
+                </div>
+                <div className="div7-drvreq">
+                  <div className="div7table-drvreq">
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td className="table-label-drvreq">
+                          <p className="header-label-drvreq">Departure Date and Time:</p>
+                        </td>
+                        <td>
+                          <p className="drvreq-details">
+                          {selectedRequest.departure_time}</p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="table-label-drvreq">
+                          <p className="header-label-drvreq">Arrival Date and Time:</p>
+                        </td>
+                        <td>
                         <p className="drvreq-details">
-                        {selectedRequest.departure_time}</p>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="table-label-drvreq">
-                        <p className="header-label-drvreq">Arrival Date and Time:</p>
-                      </td>
-                      <td>
-                      <p className="drvreq-details">
-                        {selectedRequest.arrival_time}E</p>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="table-label-drvreq">
-                        <p className="header-label-drvreq">Destination:</p>
-                      </td>
-                      <td>
-                      <p className="drvreq-details">
-                      {selectedRequest.destination}</p>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-                </div>
-              </div>
-              <div className="div9-drvreq">
-                <p className="schedlabel-drvreq">OTHER DETAILS</p>
-              </div>
-              <div className="div10-drvreq">
-              <div className="div10table-drvreq">
-                <table>
-                  <tbody>
-                    <tr>
-                      <td className="table-label-drvreq">
-                        <p className="header-label-drvreq">Total No. of Passenger/s:</p>
-                      </td>
-                      <td>
+                          {selectedRequest.arrival_time}E</p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="table-label-drvreq">
+                          <p className="header-label-drvreq">Destination:</p>
+                        </td>
+                        <td>
                         <p className="drvreq-details">
-                          {selectedRequest.passenger_count}</p>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="table-label-drvreq">
-                        <p className="header-label-drvreq">Name of Passenger/s:</p>
-                      </td>
-                      <td>
-                      <p className="drvreq-details">
-                      {selectedRequest.passenger_names && Array.isArray(selectedRequest.passenger_names) && selectedRequest.passenger_names.map((passenger, index) => (
-                        <div key={index}>{passenger}</div>
-                      ))}
-                      </p>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="table-label-drvreq">
-                        <p className="header-label-drvreq">Purpose:</p>
-                      </td>
-                      <td>
-                      <p className="drvreq-details">
-                        {selectedRequest.purpose}</p>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="table-label-drvreq">
-                        <p className="header-label-drvreq">Requested By:</p>
-                      </td>
-                      <td>
-                      <p className="drvreq-details">
-                      {selectedRequest.requested_by}</p>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
+                        {selectedRequest.destination}</p>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  </div>
                 </div>
-              </div>
-              <div className="div11-drvreq">
-                <Button variant="contained" color="primary" style={{ textTransform: 'none', backgroundColor: '#025BAD' }}>
-                  Generate Trip Ticket
-                </Button>
-              </div>
+                <div className="div9-drvreq">
+                  <p className="schedlabel-drvreq">OTHER DETAILS</p>
+                </div>
+                <div className="div10-drvreq">
+                <div className="div10table-drvreq">
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td className="table-label-drvreq">
+                          <p className="header-label-drvreq">Total No. of Passenger/s:</p>
+                        </td>
+                        <td>
+                          <p className="drvreq-details">
+                            {selectedRequest.passenger_count}</p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="table-label-drvreq">
+                          <p className="header-label-drvreq">Name of Passenger/s:</p>
+                        </td>
+                        <td>
+                        <p className="drvreq-details">
+                        {selectedRequest.passenger_names && Array.isArray(selectedRequest.passenger_names) && selectedRequest.passenger_names.map((passenger, index) => (
+                          <div key={index}>{passenger}</div>
+                        ))}
+                        </p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="table-label-drvreq">
+                          <p className="header-label-drvreq">Purpose:</p>
+                        </td>
+                        <td>
+                        <p className="drvreq-details">
+                          {selectedRequest.purpose}</p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="table-label-drvreq">
+                          <p className="header-label-drvreq">Requested By:</p>
+                        </td>
+                        <td>
+                        <p className="drvreq-details">
+                        {selectedRequest.requested_by}</p>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  </div>
+                </div>
+                <div className="div11-drvreq">
+                  <Button onClick={handleGenerateTripTicket} variant="contained" color="primary" style={{ textTransform: 'none', backgroundColor: '#025BAD' }}>
+                    Generate Trip Ticket
+                  </Button>
+                </div>
             </div>
-        </DialogContent>
-      </Dialog>
-    </div>
+          </DialogContent>
+        </Dialog>
+      </div>
     </ThemeProvider>
   );
 }
